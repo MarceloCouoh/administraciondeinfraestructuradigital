856877de7f5559a2f2fbc7c5f8b2ef2a248fafef
MarceloCouoh <60004525+MarceloCouoh@users.noreply.github.com>
2021-06-01 17:35:10 -0500

    Create FuncionesExternas.py

---
+++ FuncionesExternas.py
@@ -0,0 +1,12 @@
+#Declaración de fucnciones
+
+#Funsiones sin parametros
+def funcion1():
+    #contenido de la funcion
+    print ("Has llamado a la funcion 1")
+
+#Función con parametros y retorno
+def funcion2 (a,b):
+    #Contenido de la función
+    c = int(a) + int(b)
+    return c
